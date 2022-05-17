7160929a8e614d6724324b496794fb42830ec533
toshiaki-suzuki <flaflabell@gmail.com>
2022-05-17 18:13:17 +0900

    use header component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
   <div>
+    <Head>
+      <title>First Post</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <h1>First Post</h1>
     <Link href="/">
       <a>Back to home</a>
