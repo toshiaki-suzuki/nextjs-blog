37cb24c93106037e24f1a6c2dccee7cb317aea58
toshiaki-suzuki <flaflabell@gmail.com>
2022-05-17 18:24:18 +0900

    make common parts for all pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,20 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
   <div>
-    <Head>
-      <title>First Post</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <h1>First Post</h1>
-    <Link href="/">
-      <a>Back to home</a>
-    </Link>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>First Post</h1>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </Layout>
   </div>
   ) 
 }
