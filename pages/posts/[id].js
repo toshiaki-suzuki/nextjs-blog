1a3077395e1ef37a979a611e402fcdcdf3e4c3ba
toshiaki-suzuki <flaflabell@gmail.com>
2022-05-22 10:23:25 +0900

    add markdown in each post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,7 +10,8 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -26,6 +27,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
