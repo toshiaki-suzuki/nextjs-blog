44d6aced2491c726b4e596aa1843c929e1bcd19e
toshiaki-suzuki <flaflabell@gmail.com>
2022-05-22 10:29:55 +0900

    add css to each post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,8 @@ import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Layout from '../../components/layout';
 import Head from 'next/head';
+// Add this import at the top of the file
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -26,16 +28,15 @@ export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-        <title>{postData.id}</title>
+        <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {/* Replace {postData.date} with this */}
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
