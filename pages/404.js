e620a7c86f90c492133d67f58534b94118e028dc
toshiaki-suzuki <flaflabell@gmail.com>
2022-05-22 11:35:44 +0900

    customize 404 page

---
+++ pages/404.js
@@ -0,0 +1,37 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  )
+}
